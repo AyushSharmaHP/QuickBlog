267307475b06debbbf628a75fa4c92a020c9e9f6
JAVAFULLSTACK32 <ayushsharma16122004@gmail.com>
2025-06-13 11:27:14 +0530

    footer section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import Navbar from '../components/Navbar.jsx'
-import Header from '../components/Header.jsx'
-import BlogList from '../components/BlogList.jsx'
+import React from 'react';
+import Navbar from '../components/Navbar.jsx';
+import Header from '../components/Header.jsx';
+import BlogList from '../components/BlogList.jsx';
+import Newsletter from '../components/Newsletter.jsx';
+import Footer from '../components/Footer.jsx';
 
 const Home = () => {
   return (
@@ -9,8 +11,10 @@ const Home = () => {
       <Navbar />
       <Header />
       <BlogList />
+      <Newsletter />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
